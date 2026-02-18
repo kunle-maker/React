c0554b3df5bd3ec307f437ba00443cc5e45c9a1b
Kunle the one <kunletheone@gmail.com>
2026-02-18 03:28:24 +0100

    Add files via upload

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import { FiSearch, FiUserPlus, FiCheck } from 'react-icons/fi';
+import { FiSearch, FiUserPlus, FiCheck, FiPlay } from 'react-icons/fi';
 import API from '../utils/api';
 
 const Search = () => {
+  const navigate = useNavigate(); 
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [postResults, setPostResults] = useState([]);
